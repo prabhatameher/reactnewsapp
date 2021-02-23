efad766587653f077fdd698caae1061c0e15557a
prabhatameher <meher.prabhat1996@gmail.com>
2021-02-24 00:10:49 +0530

    Updates

---
+++ precache-manifest.ef6bf31a384e6541e1e18717f2e1e445.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33060e73784ddc5ed80dcd6f983cec29",
+    "url": "/reactnewsapp/index.html"
+  },
+  {
+    "revision": "91423d49aeef39597796",
+    "url": "/reactnewsapp/static/css/main.cae59f50.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/reactnewsapp/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactnewsapp/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91423d49aeef39597796",
+    "url": "/reactnewsapp/static/js/main.35278b2e.chunk.js"
+  },
+  {
+    "revision": "2fe63da6a8b1a7243e8a",
+    "url": "/reactnewsapp/static/js/runtime-main.f276f39f.js"
+  }
+]);
